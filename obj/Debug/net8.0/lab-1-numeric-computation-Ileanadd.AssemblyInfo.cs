b301b926a48eba324da182bffabfde1ee8e2d99b
Ileana Gonzalez <ileanagonzalez@Ileanas-MacBook-Pro.local>
2026-05-21 14:07:31 -0600

    Example 4, finished step: 4

--- obj/Debug/net8.0/lab-1-numeric-computation-Ileanadd.AssemblyInfo.cs
+++ obj/Debug/net8.0/lab-1-numeric-computation-Ileanadd.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab-1-numeric-computation-Ileanadd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e039f70437deebffc714e847f9125d9464ffbe06")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c5884c5d2b1304b66b824951f75b8fa4f89ad10")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab-1-numeric-computation-Ileanadd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab-1-numeric-computation-Ileanadd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
