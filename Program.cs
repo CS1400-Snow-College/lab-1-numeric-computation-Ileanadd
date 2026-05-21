5c5c1dc49ecc1c8945a04be1b94c755037c4712c
Ileana Gonzalez <ileanagonzalez@Ileanas-MacBook-Pro.local>
2026-05-21 12:58:57 -0600

    Example 1, finished step: 5

--- Program.cs
+++ Program.cs
@@ -5,7 +5,17 @@ void WorkWithIntegers() {
     int a = 18;
     int b = 6;
     int c = a + b;
-
+    Console.WriteLine(c);
+    
+    // substraction
+    c = a - b;
     Console.WriteLine(c);
 
+    // multiplication
+    c = a * b;
+    Console.WriteLine(c);
+
+    // division
+    c = a / b;
+    Console.WriteLine(c);
 }
